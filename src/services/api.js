3de10adf661e6af5c30f78aff9eabbf6885d226e
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-11-22 19:02:19 -0300

    requisições de api criadas

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+export const getCategories = () => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  return fetch(url).then((response) => response.json());
+};
+
+export const getProductsFromQuery = (query) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  return fetch(url).then((response) => response.json());
+};
+
+export const getProductsFromCategory = (categoryId) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  return fetch(url).then((response) => response.json());
+};
+
+export const getProductsFromId = (productId) => {
+  const url = `https://api.mercadolibre.com/items/${productId}`;
+  return fetch(url).then((response) => response.json());
+};
