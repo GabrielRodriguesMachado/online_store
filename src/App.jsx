86638fcc85b50b803daea1100d2c1a9ffa9a37cb
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-11-22 19:15:38 -0300

    criada rota para products

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Products from './pages/Products';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/products" element={<Products />} />
         <Route path="/" element={<Login />} />
+        <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </BrowserRouter>
   );
