128ea9acbaafa168861671e222be5b3198998d86
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-11-28 15:05:33 -0300

    criada a interação com as categorias

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -8,13 +8,21 @@ function Categories() {
     getCategories().then((data) => setCategories(data));
   }, []);
 
+  const teste = (id) => {
+    console.log(id);
+  };
+
   return (
     <div>
       <aside>
         <h2>Categorias</h2>
         <ul>
           {categories.map((category) => (
-            <li key={category.id}>{category.name}</li>
+            <li key={category.id}>
+              <button type="button" onClick={() => teste(category.id)}>
+                {category.name}
+              </button>
+            </li>
           ))}
         </ul>
       </aside>
