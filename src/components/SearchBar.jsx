c76de452ce1c410e202d558f1dfd75914dbafacd
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-11-28 14:56:10 -0300

    search bar estilizada

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,14 @@ function SearchBar() {
       <input
         type="text"
         placeholder="Search"
+        className="appearance-none block px-4 leading-tight text-gray-700 border border-gray-300 rounded-md py-3 mx-2 focus:outline-none focus:border-gray-500 focus:shadow-outline-gray"
       />
+      <button
+        type="button"
+        className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out"
+      >
+        Search
+      </button>
     </div>
   );
 }
